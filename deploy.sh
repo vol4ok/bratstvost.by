7baee4a8bd7b9f55c887ce434bd4dca6ff263406
sergey-tsarevich <sergey.tsarevich@gmail.com>
2014-06-09 03:27:27 +0300

    Set image height.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 grunt rebuild
 cd dist
 git add .
-git ci -a -m "up "`date +"%d.%m.%Y-%H:%M:%S"`
+git commit -a -m "up "`date +"%d.%m.%Y-%H:%M:%S"`
 git push origin master
 ssh root@vol4ok.com << 'ENDSSH'
 cd /var/www/bratstvost
